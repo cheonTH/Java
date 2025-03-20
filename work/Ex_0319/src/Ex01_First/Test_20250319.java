48010a8909756d8fcc6c245c37575cc524f3e628
cheonTH <xogus0530@gmail.com>
2025-03-20 10:13:07 +0900

    init

---
+++ work/Ex_0319/src/Ex01_First/Test_20250319.java
@@ -0,0 +1,18 @@
+package Ex01_First;
+
+public class Test_20250319 {
+	// main함수
+	// 파일을 실행하면 main함수 안에 있는 코드가 실행이 됨
+	// main 템플릿 -> main작성 후 ctrl + space
+	public static void main(String[] args) {
+		//TODO: 해야할 일이 있습니다.  
+		//ㄴ>아직 해결되지 않은 작업이나 구현해야 할 부분을 나타내는 주석 태그
+		//FIXME: 해당 코드에서는 런타임 오류가 발생합니다 수정해야합니다
+		//ㄴ> 수정이 필요한 버그, 오류, 또는 잘못된 동작을 나타내는 주석태그, 보통 어떤 문제가 있는지 설명하고 이를 해결해야하는 이유를 기록
+		//XXX: 해당부분이 하드코딩 된 것 같습니다. 수정해야 합니다.
+		//ㄴ>다른 개발자가 알아봐야 할 중요한 이슈 또는 경고		
+		
+		
+		System.out.println("Hello, World");
+	}
+}
